8f585e6ed48e23f9f0ba233ffd3cfed1a63ca2f0
Mercedes Coyle <mcoyle@realgravity.com>
2013-10-25 18:25:17 -0700

    randomizing the fading, removed a lot of dead code

--- fade_dress.ino
+++ fade_dress.ino
@@ -1,65 +1,29 @@
-/* This sketch is a simple series of loops which use the analogWrite function to gradually fade 5 LEDs.
-   There's a bit of copy/paste here - I may clean it up in the future, but for now I'm doing other projects. */
-
-int ledPin5 = 5;                // bottom LED connected to pin 5
-int ledPin6 = 6;                // 2nd from bottom LED connected to pin 6
-int ledPin9 = 9;                // 3rd from bottom LED conneted to pin 9
-int ledPin10 = 10;              // 4th from bottom LED connected to pin 10
-int ledPin11 = 11;              // top LED connected to pin 11
-int ledPin13 = 13;              // board LED
+int ledArray[] = {5, 6, 9, 10, 11};
+int arrayLength = 5;
 
 void setup(){
+  for(int count = 0; count < arrayLength; count ++) {
+    digitalWrite(ledArray[count], HIGH);
+    delay(10);
+    digitalWrite(ledArray[count], LOW);
+    delay(10);
+  }
+
 }
 
-void loop() {                                                                                                                           
-  // LED 5
-  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-    analogWrite(ledPin5, fadeValue);
-    delay(7);
-  }
-  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-    analogWrite(ledPin5, fadeValue);
-    delay(7);
-  }
-
-  // LED 6
-  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-    analogWrite(ledPin6, fadeValue);
-    delay(7);
-  }
-  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-    analogWrite(ledPin6, fadeValue);
-    delay(7);
-  }
-
-  // LED 7
-  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-    analogWrite(ledPin9, fadeValue);
-    delay(7);
-  }
-  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-    analogWrite(ledPin9, fadeValue);
-    delay(7);
-  }
-
-  // LED 10
-  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-    analogWrite(ledPin10, fadeValue);
-    delay(7);
-  }
-  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-    analogWrite(ledPin10, fadeValue);
-    delay(7);
-  }
-
-  // LED 11
-  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-    analogWrite(ledPin11, fadeValue);
-    delay(7);
-  }
-  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-    analogWrite(ledPin11, fadeValue);
-    delay(20);
-  }
+void loop(){   
+  
+  for (int count = 0; count < arrayLength; count ++) {
+    int randLED = random(0, arrayLength);
+    int randTime = random(0, 20);
+    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
+      analogWrite(ledArray[randLED], fadeValue);
+      delay(randTime);
+    }
+    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
+      analogWrite(ledArray[randLED], fadeValue);
+      delay(randTime);
+    }
+  }                                                                                                           
 }
 
